9d44078bdf69ced32584955a17c4dfc72810b8d9
taahirahismail <taahirah.i@gmail.com>
2023-06-26 14:24:48 +0200

    what am I doing lol

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <p>
+      <span :title="message">Hover and see the dynamic title.</span>
+    </p>
+    <p :class="{red: isRed}" @click="toggleRed()">
+      This should be red, but click me to toggle.
+    </p>
+    <p :style="{color}" @click="toggleColor()">
+      This will change color when clicking.
+    </p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      message: 'Hello There',
+      isRed: true,
+      color: 'green',
+    }
+  },
+  
+  methods: {
+    toggleRed() {
+      this.isRed = !this.isRed
+    },
+    toggleColor() {
+      this.color = this.color === 'green'?'blue':'green'
+    }
+  },
 }
 </script>
+
+<style>
+.red {color: red;}
+</style>
